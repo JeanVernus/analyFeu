00804d82747dd969b5cdce74cbb9185446b561da
Enigmhatik <jeannywcs@gmail.com>
2019-06-12 16:40:34 +0200

    contactMe

--- back/index.js
+++ back/index.js
@@ -9,7 +9,41 @@ App.use(Cors());
 App.use(BodyParser.json());
 App.use(BodyParser.urlencoded({ extended: true }));
 
+App.post('/sendForm', (req, res) => {
+  console.log('req.body', req.body);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const company = req.body.company;
+  const Text = req.body.Text;
+  const Email = req.body.Email;
+  const nodemailer = require("nodemailer");
 
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+      type: "login",
+      user: "",
+      pass: ""
+    }
+  });
+  let mailOptions = {
+    from: `${Email}`,
+    to: '"JeanVernus"<jeannywcs@gmail.com>',
+    subject: "Portefolio",
+    text: "",
+    html: `${firstName}"..."${lastName}<br />${job}"..."${company}<br />${Text}`,
+  };
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err.message);
+      return process.exit(1)
+    }
+    else {
+      console.log(info);
+    }
+  });
+})
 
 App.listen(Port, () => {
   console.log(`server started on ${Port}`);
--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ContactMe from './Components/ContactMe';
 import './CSS/App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <ContactMe/>
       </div>
     );
   }
