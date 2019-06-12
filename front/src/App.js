7570db5d0db555d844bdd0a6d8254bb2fc40e697
Luteces <ebompuis.dev@gmail.com>
2019-06-12 16:37:28 +0200

    Debug

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import './CSS/App.css';
+import React, { Component } from "react";
+import "./CSS/App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
-        <h2>Hello</h2>
+        <h1>Hello wor</h1>
+        <h2>Hell</h2>
+        <h3>Heyo</h3>
       </div>
     );
   }
