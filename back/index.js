3b7822bdcb3b0c5dcc7cadb969fc70b979c38d4a
Enigmhatik <jeannywcs@gmail.com>
2019-06-12 15:50:08 +0200

    statProject

---
+++ back/index.js
@@ -0,0 +1,16 @@
+const Express = require('express');
+const Cors = require('cors');
+const BodyParser = require('body-parser');
+
+const Port = 5000;
+
+const App = Express();
+App.use(Cors());
+App.use(BodyParser.json());
+App.use(BodyParser.urlencoded({ extended: true }));
+
+
+
+App.listen(Port, () => {
+  console.log(`server started on ${Port}`);
+})
