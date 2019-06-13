5bd3c2c02be2225dbdb3cf89a99d4d84de49c079
Luteces <ebompuis.dev@gmail.com>
2019-06-13 12:27:39 +0200

    Add footer

---
+++ front/src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import '../CSS/Footer.css'
+
+class FooterBar extends Component {
+
+    render() {
+        return (
+            <div id={"FOOTER"}>
+                   <h5>Footer</h5>
+            </div>
+        );
+    }
+
+}
+export default FooterBar;
