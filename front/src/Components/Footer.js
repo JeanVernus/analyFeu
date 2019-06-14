7e43ad1c94a466dbeeff21869a72dd9b9994af15
Luteces <ebompuis.dev@gmail.com>
2019-06-14 10:52:43 +0200

    Footer UP

--- front/src/Components/Footer.js
+++ front/src/Components/Footer.js
@@ -1,15 +1,49 @@
-import React, { Component } from "react";
-import '../CSS/Footer.css'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+// import Button from "@material-ui/core/Button";
+// import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
+import { red } from "@material-ui/core/colors";
+import "../CSS/Navbar.css";
+import "../CSS/Footer.css";
+import { Box } from "@material-ui/core";
 
-class FooterBar extends Component {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
-    render() {
-        return (
-            <div id={"FOOTER"}>
-                   <h5>Footer</h5>
-            </div>
-        );
-    }
+export default function FooterBar() {
+  const classes = useStyles();
 
+  return (
+    <div id={"FOOTER"}>
+      <div className={classes.root}>
+        <AppBar position="static" style={{ backgroundColor: red[900] }}>
+          <Box order={2} alignItems={"flex-start"}>
+            <Typography>
+              <strong>
+                ANALYFEU <br /> 9 Chemin de la clauvade <br /> 89390 CUERS
+              </strong>
+              <br />
+              <strong>Tel : 06 06 06 06 06</strong>
+            </Typography>
+          </Box>
+          <Box order={1} alignItems={"flex-end"}>
+            <Typography>Test</Typography>
+          </Box>
+        </AppBar>
+      </div>
+    </div>
+  );
 }
-export default FooterBar;
--- front/src/Components/Navbar.js
+++ front/src/Components/Navbar.js
@@ -1,47 +1,53 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import {red} from "@material-ui/core/colors";
-import '../CSS/Navbar.css'
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { red } from "@material-ui/core/colors";
+import "../CSS/Navbar.css";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
 export default function NavBarMaterial() {
   const classes = useStyles();
 
   return (
-      <div className={classes.root}>
-        <AppBar position="static" style={{backgroundColor: red[900]}} >
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              <a href={'/'} className={"LINK_HOVER"} color={"inherit"}>
+    <div className={classes.root}>
+      <AppBar position="static" style={{ backgroundColor: red[900] }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <a href={"/"} className={"LINK_HOVER"} color={"inherit"}>
               Analyfeu
-              </a>
-            </Typography>
-            <Button color="inherit">Bureau d'étude/Coordination SSI</Button>
-            <Button color="inherit">Formations</Button>
-            <Button className={"LINK_HOVER"} href={"/contact"} color="inherit">Références contact</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
+            </a>
+          </Typography>
+          <Button color="inherit">Bureau d'étude/Coordination SSI</Button>
+          <Button color="inherit">Formations</Button>
+          <Button className={"LINK_HOVER"} href={"/contact"} color="inherit">
+            Références contact
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
